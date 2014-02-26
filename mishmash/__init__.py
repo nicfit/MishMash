8c441b277a1810e4aacce7307a11c5a02c80cdb8
nicfit <travis@pobox.com>
2014-02-25 19:58:45 -0700

    Remove info.py

--- mishmash/__init__.py
+++ mishmash/__init__.py
@@ -21,6 +21,17 @@
 __author__ = 'Travis Shirk'
 __email__ = 'travis@pobox.com'
 __version__ = '0.1.0'
+__version_info__   = tuple((int(v) for v in __version__.split('.')))
 __release__ = 'alpha'
+__web__ = "http://mishmash.nicfit.net/"
+
+__version_txt__     = """
+MishMash %(__version__)s-%(__release__)s (C) Copyright %(__author__)s
+This program comes with ABSOLUTELY NO WARRANTY! See LICENSE for details.
+Run with --help/-h for usage information or read the docs at
+%(__web__)s
+""" % (locals())
+
+__years__ = "2012-2014"
 
 from .log import log
--- tests/test_mishmash.py
+++ tests/test_mishmash.py
@@ -21,7 +21,11 @@ class TestMishmash(unittest.TestCase):
         assert(mishmash.__author__)
         assert(mishmash.__email__)
         assert(mishmash.__version__)
+        assert(mishmash.__version_info__)
+        assert(mishmash.__version_txt__)
         assert(mishmash.__release__)
+        assert(mishmash.__web__)
+        assert(mishmash.__years__)
 
     def tearDown(self):
         pass
