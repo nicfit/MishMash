ab0f2d89eee202e2796f98a66532d755dc2e7897
Travis Shirk <travis@pobox.com>
2014-10-09 10:51:17 -0600

    Messing with tests.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,32 @@
 # -*- coding: utf-8 -*-
+import os
+import unittest
+from pyramid import testing
+import mishmash.database
+
+settings = {
+    'sqlalchemy.url': 'sqlite:///mishmash.test.db',
+}
+"""Bare minimum settings required for testing."""
+
+
+class BaseTestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        (cls.engine,
+         cls.session) = mishmash.database.init(settings['sqlalchemy.url'],
+                                               drop_all=True)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.session.close()
+
+    def setUp(self):
+        request = testing.DummyRequest()
+        self.config = testing.setUp(request=request)
+
+        self.config.add_route('home', '/')
+        self.config.add_settings(settings)
+
+    def tearDown(self):
+        testing.tearDown()
