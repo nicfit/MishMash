bab2d15990d2beb857f1881f1a73e03863f97b37
nicfit <travis@pobox.com>
2014-10-09 10:51:17 -0600

    Messing with tests.

--- tests/test_mishmash.py
+++ tests/test_mishmash.py
@@ -7,29 +7,25 @@ test_mishmash
 Tests for `mishmash` module.
 """
 
-import unittest
-
 import mishmash
+from . import BaseTestCase
 
 
-class TestMishmash(unittest.TestCase):
-
-    def setUp(self):
-        pass
+class TestMishmash(BaseTestCase):
 
     def test_metadata(self):
         assert(mishmash.__author__)
         assert(mishmash.__email__)
         assert(mishmash.__version__)
         assert(mishmash.__version_info__)
+        assert(type(mishmash.__version_info__) is tuple)
+        assert(len(mishmash.__version_info__) == 3)
         assert(mishmash.__version_txt__)
         assert(mishmash.__release__)
         assert(mishmash.__web__)
         assert(mishmash.__years__)
 
-    def tearDown(self):
-        pass
-
-
-if __name__ == '__main__':
-    unittest.main()
+    def test_log_import(self):
+        import logging
+        assert("log" in dir(mishmash))
+        assert(isinstance(mishmash.log, logging.Logger))
