f5afa04be7f1fa8f2fe82329893a335ec8e61f1f
Travis Shirk <travis@pobox.com>
2017-02-12 23:25:43 -0700

    version bump and new version code from somewhere, else.  !wip

--- mishmash/__about__.py
+++ mishmash/__about__.py
@@ -1,8 +1,17 @@
 # -*- coding: utf-8 -*-
 from collections import namedtuple
 
-__version__ = "0.2-b5"
-__release_name__ = ""
+
+def __parse_version(v):
+    for c in ("a", "b", "c"):
+        parsed = v.split(c)
+        if len(parsed) == 2:
+            return (parsed[0], c + parsed[1])
+    return v, "final"
+
+
+__version__ = "0.3b0"
+__release_name__ = "I Cant Quit You Baby"
 __years__ = "2013-2017"
 
 __project_name__ = "MishMash"
@@ -16,8 +25,8 @@ __long_description__ = ""
 __license__ = "GNU GPL v3.0"
 __github_url__ = "https://github.com/nicfit/mishmash",
 
-__release__ = __version__.split("-")[1] if "-" in __version__ else "final"
-_v = tuple((int(v) for v in __version__.split("-")[0].split(".")))
+__release__ = __parse_version(__version__)[1]
+_v = tuple((int(v) for v in __parse_version(__version__)[0].split(".")))
 __version_info__ = \
     namedtuple("Version", "major, minor, maint, release")(
         *(_v + (tuple((0,)) * (3 - len(_v))) +
