661522628c9032257b7d034423d85c468147871a
Travis Shirk <travis@pobox.com>
2017-11-25 18:39:45 -0700

    fix: PServeCommand requires .ini extension

--- mishmash/commands/web.py
+++ mishmash/commands/web.py
@@ -12,10 +12,10 @@ if MISHMASH_WEB:
         HELP = "MishMash web interface."
 
         def _run(self):
-            # pserve wants a file to open, so use the *composed* config.
-            with tempfile.NamedTemporaryFile(mode="w") as config_file:
+            # pserve wants a file to open, so use the composed config.
+            with tempfile.NamedTemporaryFile(mode="w",
+                                             suffix=".ini") as config_file:
                 self.config.write(config_file)
                 config_file.flush()
-                argv = ["mishmish", config_file.name]
-                pserve = PServeCommand(argv)
+                pserve = PServeCommand(["mishmish", config_file.name])
                 return pserve.run()
