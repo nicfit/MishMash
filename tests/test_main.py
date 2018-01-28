34e438bf6ce5cb99e72a1ada0c1f0de4eadb3afc
Travis Shirk <travis@pobox.com>
2018-01-28 15:53:04 -0700

    Removed no-arg (nicfit.py) main test, test is done upstream.

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,12 +1,2 @@
 from unittest.mock import Mock
 from mishmash.__main__ import MishMash
-
-
-def test_main_noargs():
-    app = MishMash()
-    app.arg_parser.print_help = Mock()
-
-    retval = app._run([])
-
-    app.arg_parser.print_help.assert_called()
-    assert retval == 1
