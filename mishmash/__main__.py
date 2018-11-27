348e4b9fbd5b03847e9576b4f3453d6303093eca
Travis Shirk <travis@pobox.com>
2018-11-26 18:28:18 -0700

    new: Added `MishMash(ConfigClass=clazz)` keyword argument.

--- mishmash/__main__.py
+++ mishmash/__main__.py
@@ -64,12 +64,12 @@ def main(args):
 
 
 class MishMash(Application):
-    def __init__(self, progname="mishmash"):
+    def __init__(self, progname="mishmash", ConfigClass=None):
         from . import version
         config_opts = ConfigOpts(required=False,
                                  default_config=DEFAULT_CONFIG,
                                  default_config_opt="--default-config",
-                                 ConfigClass=Config,
+                                 ConfigClass=ConfigClass or Config,
                                  config_env_var=CONFIG_ENV_VAR,
                                  init_logging_fileConfig=True)
         super().__init__(main, name=progname, version=version,
