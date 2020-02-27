a79ad96d9f7dd3f893181c636340991c9a6582ef
Travis Shirk <travis@pobox.com>
2020-02-26 22:04:16 -0700

    Remove user orm.

--- mishmash/orm/__init__.py
+++ mishmash/orm/__init__.py
@@ -1,14 +1,10 @@
 from .core import *   # noqa: F403
-from . import user
 
 # Core orm
 TYPES = [Meta, Library, Tag, Artist, Album, Track, Image]  # noqa: F405
 TAGS = [artist_tags, album_tags, track_tags, artist_images, album_images]  # noqa: F405
 IMAGE_TABLES = [artist_images, album_images]  # noqa: F405
 ENUMS = [Image._types_enum, Album._types_enum]  # noqa: F405
-
-# User orm
-TYPES += [user.User]
 
 # All the table instances. Order matters (esp. for postgresql). The tables are created in
 # normal order, and dropped in reverse order.
